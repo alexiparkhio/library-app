ee8897f1e6e67a80de33ee29c1dc2bc9a7ae5d9a
Alex Park <alexparkvinyas@gmail.com>
2020-07-06 10:09:51 +0200

    add navbar for logged users

--- hexad-library-app/src/components/commons/FontAwesomeIcons/index.js
+++ hexad-library-app/src/components/commons/FontAwesomeIcons/index.js
@@ -1,4 +1,24 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserShield, faUser, faBook, faSignOutAlt, faUserPlus, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+import { 
+    faUserShield, 
+    faUser, 
+    faBook, 
+    faSignOutAlt, 
+    faUserPlus, 
+    faSignInAlt,
+    faBookDead,
+    faStar,
+    faLightbulb,
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserShield, faUser, faBook, faSignOutAlt, faUserPlus, faSignInAlt);
+library.add(
+    faUserShield, 
+    faUser, 
+    faBook, 
+    faSignOutAlt, 
+    faUserPlus, 
+    faSignInAlt,
+    faBookDead,
+    faStar,
+    faLightbulb,
+);
